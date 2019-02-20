04e4b22c6b7706b512aee5f35c4efcc6e0d8a1bb
MaximFrolov91 <40990237+MaximFrolov91@users.noreply.github.com>
2019-02-20 13:43:54 +0200

    Update task_6.1-6.4.php

--- task_6.1-6.4.php
+++ task_6.1-6.4.php
@@ -1,26 +1,25 @@
-<?phpclass Student {
-    // constructor
-    public function __construct($firstName = "n/a", $lastName = "n/a", $gender = "n/a", $status = "freshman", $gpa = 0) {
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
+<?php
+class Student {
+	public function __construct($firstName = "n/a", $lastName = "n/a", $gender = "n/a", $status = "freshman", $gpa = 0) {
+		$this->firstName = $firstName;
+		$this->lastName = $lastName;
 		$this->gender = $gender;
 		$this->status = $status;
 		$this->gpa = $gpa;
-    }
-
-    public function showMyself() {
-        echo $this->firstName . " " . $this->lastName . " " . $this->gender . " " . $this->status . " " . $this->gpa . PHP_EOL;
-    }
+	}
+	public function showMyself() {
+		echo $this->firstName . " " . $this->lastName . " " . $this->gender . " " . $this->status . " " . $this->gpa . PHP_EOL;
+	}
 	public function studyTime($studyTime = 0.0) {
 		$this->gpa += log($studyTime);
 		if ($this->gpa > 4) {
 			$this->gpa = 4;
 		}
 		return $this->gpa;
-    }
-	
+	}
+	public function __destruct() {
+	}
 }
-
 $Mike = new Student("Mike", "Barnes", "male", "freshman", 4.0);
 $Jim = new Student("Jim", "Nickerson", "male", "sophomore", 3.0);
 $Jack = new Student("Jack", "Indabox", "male", "junior", 2.5);
